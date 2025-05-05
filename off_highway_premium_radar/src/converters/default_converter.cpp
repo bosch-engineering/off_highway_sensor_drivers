e1bb6f9310915d8b1021e684e99592e42d03cf4a
Sarah Huber <sarah.huber@de.bosch.com>
2025-05-05 17:16:49 +0200

    Adapt diagnostic messages
    Signed-off-by: Sarah Huber <sarah.huber@de.bosch.com>

--- off_highway_premium_radar/src/converters/default_converter.cpp
+++ off_highway_premium_radar/src/converters/default_converter.cpp
@@ -22,7 +22,7 @@ namespace off_highway_premium_radar
 DefaultConverter::DefaultConverter()
 : diag_frequencies_locations_{13., 17.},
   diag_frequencies_sensor_feedback_{18., 22.},
-  diag_frequencies_sensor_state_information_{90., 110.}
+  diag_frequencies_sensor_state_information_{13., 17.}
 {
 }
 
@@ -60,12 +60,13 @@ void DefaultConverter::on_configure()
     publisher_locations_->get_topic_name(), *diag_updater_,
     FrequencyStatusParam(&diag_frequencies_locations_.min, &diag_frequencies_locations_.max),
     TimeStampStatusParam(diag_timestamps_.min, diag_timestamps_.max));
-  diag_sensor_feedback_ = std::make_shared<diagnostic_updater::TopicDiagnostic>(
-    publisher_sensor_feedback_->get_topic_name(), *diag_updater_,
-    FrequencyStatusParam(
-      &diag_frequencies_sensor_feedback_.min,
-      &diag_frequencies_sensor_feedback_.max),
-    TimeStampStatusParam(diag_timestamps_.min, diag_timestamps_.max));
+  //TODO(shu-beg) Add sensor feedback diagnostics again when PDU is available
+  // diag_sensor_feedback_ = std::make_shared<diagnostic_updater::TopicDiagnostic>(
+  //   publisher_sensor_feedback_->get_topic_name(), *diag_updater_,
+  //   FrequencyStatusParam(
+  //     &diag_frequencies_sensor_feedback_.min,
+  //     &diag_frequencies_sensor_feedback_.max),
+  //   TimeStampStatusParam(diag_timestamps_.min, diag_timestamps_.max));
   diag_sensor_state_information_ = std::make_shared<diagnostic_updater::TopicDiagnostic>(
     publisher_sensor_state_information_->get_topic_name(), *diag_updater_,
     FrequencyStatusParam(
@@ -124,7 +125,8 @@ void DefaultConverter::on_sensor_feedback(const SensorFeedback & data)
 {
   auto stamp = decide_on_stamp(data.FeedBack_TimeS, data.FeedBack_TimeNs);
 
-  publish_tick_diag(data, publisher_sensor_feedback_, diag_sensor_feedback_, stamp);
+  //TODO(shu-beg) Add sensor feedback diagnostics again when PDU is available
+  //publish_tick_diag(data, publisher_sensor_feedback_, diag_sensor_feedback_, stamp);
 }
 
 void DefaultConverter::on_sensor_state_information(const SensorStateInformation & data)
