2405e33d022711c4b5a8b326fb4e161b4960cf1a
Gabriela Adriana Lapuste <gabriela-adriana.lapuste@ro.bosch.com>
2026-01-19 10:38:30 +0200

    Add force diagnostic update on timeout state transition
    Signed-off-by: Gabriela Adriana Lapuste <gabriela-adriana.lapuste@ro.bosch.com>

--- off_highway_can/include/off_highway_can/impl/receiver_impl.hpp
+++ off_highway_can/include/off_highway_can/impl/receiver_impl.hpp
@@ -52,6 +52,11 @@ void Receiver::callback_can(const typename Msg::ConstSharedPtr & frame)
 
   last_message_received_ = now();
 
+  if (is_timeout_) {
+    is_timeout_ = false;
+    force_diag_update();
+  }
+
   auto header = frame->header;
   header.frame_id = node_frame_id_;
   process(header, id, msg);
